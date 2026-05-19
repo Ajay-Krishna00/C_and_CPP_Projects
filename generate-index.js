23805d3413ce12d97863ddc95aeb1e49abfbcb07
Ajay-Krishna00 <enthusiastajay00@gmail.com>
2026-05-20 02:49:15 +0530

    auto generate file index

---
+++ generate-index.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+
+const PUBLIC_DIR = path.join(__dirname, "public");
+
+function scan(dir, base = "") {
+    let result = {};
+
+    const items = fs.readdirSync(dir);
+
+    for (const item of items) {
+        const fullPath = path.join(dir, item);
+        const relativePath = path.join(base, item);
+
+        if (fs.statSync(fullPath).isDirectory()) {
+            result[item] = scan(fullPath, relativePath);
+        } else {
+            result[item] = "file";
+        }
+    }
+
+    return result;
+}
+
+const structure = scan(PUBLIC_DIR);
+
+fs.writeFileSync(
+    path.join(PUBLIC_DIR, "index.json"),
+    JSON.stringify(structure, null, 2)
+);
+
+console.log("index.json generated");

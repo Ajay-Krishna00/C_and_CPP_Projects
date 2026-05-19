fa3ea1fb95d481c84a967d450119d64b43725a21
Ajay-Krishna00 <enthusiastajay00@gmail.com>
2026-05-20 02:45:30 +0530

    fix api path

--- api/list.js
+++ api/list.js
@@ -1,22 +1,29 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-module.exports = (req, res) => {
-  const basePath = path.join(process.cwd(), "public");
+module.exports = async (req, res) => {
+    try {
+        const publicDir = path.join(__dirname, "..", "public");
 
-  function scan(dir) {
-    const results = [];
-    const items = fs.readdirSync(dir);
-    items.forEach(item => {
-      const fullPath = path.join(dir, item);
-      if (fs.statSync(fullPath).isDirectory()) {
-          result[item] = scan(fullPath);
-      } else {
-          result[item] = "file";
-      }
-    })
-  }
-  const files = scan(basePath);
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).json(files);
-}
+        function scan(dir) {
+            const result = {};
+            const items = fs.readdirSync(dir);
+            for (const item of items) {
+                const fullPath = path.join(dir, item);
+                if (fs.statSync(fullPath).isDirectory()) {
+                    result[item] = scan(fullPath);
+                } else {
+                    result[item] = "file";
+                }
+            }
+            return result;
+        }
+        const structure = scan(publicDir);
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).json(structure);
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        });
+    }
+};
